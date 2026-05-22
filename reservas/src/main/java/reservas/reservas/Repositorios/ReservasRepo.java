db77652ed84acdcd4b2e7b7bfc627c604c055f67
Alex <alexcomesanagz@gmail.com>
2026-05-22 17:28:58 +0200

    fin.

--- reservas/src/main/java/reservas/reservas/Repositorios/ReservasRepo.java
+++ reservas/src/main/java/reservas/reservas/Repositorios/ReservasRepo.java
@@ -2,6 +2,8 @@ package reservas.reservas.Repositorios;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import reservas.reservas.Entidades.Reserva;
 import reservas.reservas.Enums.EstadoReserva;
@@ -12,4 +14,8 @@ import java.util.List;
 public interface ReservasRepo extends JpaRepository<Reserva, Long> {
     List<Reserva> findByUsuario(int usuario);
     List<Reserva> findByEstado(EstadoReserva estado);
+
+    @Query("select count(r) > 0 from Reserva r where r.habitacion.hotel.hotel_id = :hotelId")
+    boolean existsByHotelId(@Param("hotelId") int hotelId);
+
 }
